fc59bac8cb34355478996301af7dd7eddaabac3b
Dan Mee <2661185+danmee10@users.noreply.github.com>
2025-03-20 20:19:48 -0600

    project search spacing

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -32,7 +32,7 @@ export default function ProjectsPage() {
   return (
     <div className="max-w-4xl mx-auto p-4 bg-gray-50">
       <div className="flex flex-col">
-        <div className="flex justify-between items-center mb-4">
+        <div className="flex justify-between items-center mb-4 gap-2">
           <SearchBar
             search={search}
             setSearch={setSearch}
